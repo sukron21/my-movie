9f08e7b0679a5ab0dbc9565465fed9219054d67e
sukron21 <rahmatfurqon0@gmail.com>
2025-06-20 22:23:37 +0700

    penyesuaian spinner

--- src/component/atoms/Spinner.tsx
+++ src/component/atoms/Spinner.tsx
@@ -1,15 +1,5 @@
 import { Spin } from "antd";
 
-const contentStyle: React.CSSProperties = {
-  padding: 50,
-  borderRadius: 4,
-};
-const content = <div style={contentStyle} />;
-
-const Spinner: React.FC = () => (
-  <Spin tip="Loading" size="large">
-    {content}
-  </Spin>
-);
+const Spinner: React.FC = () => <Spin tip="Loading" size="large" />;
 
 export default Spinner;
